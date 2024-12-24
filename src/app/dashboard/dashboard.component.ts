28b88229f2400f3d2c6bab985457c16c94a6e8a5
paridestomeo <paridestomeo@gmail.com>
2024-12-24 22:17:17 +0100

    better looking navbars - incomplete

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Menubar } from 'primeng/menubar';
+import { Avatar } from 'primeng/avatar';
+import { Badge } from 'primeng/badge';
+import { MenuItem } from 'primeng/api';
+import { Menu } from 'primeng/menu';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [Menubar, Avatar, Badge, Menu],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class DashboardComponent {
+  topItems = [
+    {
+      
+    }
+  ];
+
+  leftItems = [
+    {
+      label: 'Constructions',
+      icon: 'pi pi-exclamation-triangle',
+      routerLink: ['/'],
+      style: { color: 'black' }
+    },
+    {
+      label: 'Reports',
+      icon: 'pi pi-file-pdf',
+      routerLink: ['/profile'],
+      style: { color: 'black' }
+    }
+  ];
+
+  logout() {
+    console.log('Logout');
+  }
 
 }
