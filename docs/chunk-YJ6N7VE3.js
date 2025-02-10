62e34b8536f0d11f96992e5ccec7e35dea71bb8f
paridestomeo <paridestomeo@gmail.com>
2025-02-10 21:43:50 +0100

    Fix: Moved build

--- docs/chunk-YJ6N7VE3.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-2DJUUAZ5.js";import"./chunk-TV227AQ4.js";import"./chunk-F3PPHHQD.js";import"./chunk-4CQFFRZE.js";import"./chunk-ODN5LVDJ.js";export{a as DashboardComponent};
