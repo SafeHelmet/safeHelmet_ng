69a20b3353750466581d78aa038d10fbc2fdfc32
paridestomeo <paridestomeo@gmail.com>
2025-02-14 15:52:19 +0100

    Fix: Moved build

--- docs/chunk-WUBBPNA6.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./chunk-6HLADHIP.js";import"./chunk-TV227AQ4.js";import"./chunk-F3PPHHQD.js";import"./chunk-4CQFFRZE.js";import"./chunk-ODN5LVDJ.js";export{a as DashboardComponent};
